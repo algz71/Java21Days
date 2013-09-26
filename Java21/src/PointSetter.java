26c4debe543377034e2c29dbadcdb695fbc5f547
algz71 <algz71@yahoo.com>
2013-09-25 22:50:11 -0400

    X is equal to

--- Java21/src/PointSetter.java
+++ Java21/src/PointSetter.java
@@ -7,7 +7,7 @@ public class PointSetter {
 		Point location = new Point(4,13);
 		
 		System.out.println("Starting location:");
-		System.out.println("X equals " + location.x);
+		System.out.println("X is equal to " + location.x);
 		System.out.println("Y equals " + location.y);
 		System.out.println("\n Moving to  (7, 6)");
 		
